1ba7b1b81d28e73a6919cb11b0a4b1be706c1018
zeke96 <15305575+moyutianque@users.noreply.github.com>
2021-08-13 03:18:32 +0800

    Update file_info.py

--- ZHUtils/utils/file_info.py
+++ ZHUtils/utils/file_info.py
@@ -16,12 +16,8 @@ display_element_prefix_last = '└──'
 display_parent_prefix_middle = '    '
 display_parent_prefix_last = '│   '
 
-def get_dict_struct(data):
-    # the data may contain data type other than default types, like tensor or ndarray
-    raise NotImplementedError
-
-def get_json_struct(data, depth=0, max_depth=20,
-                    head_name='this.json', tree_str='', prefix=''):
+def get_dict_struct(data:dict, depth=0, max_depth=20,
+                    head_name='Dict', tree_str='', prefix=''):
     # can also be used as dict structure discovery, if has large tensor or ndarray, use a modified version
     """ example structure tree
         file.json
@@ -55,23 +51,23 @@ def get_json_struct(data, depth=0, max_depth=20,
         for i, k in enumerate(list(data.keys())):
             if i < (len(data.keys())-1):
                 tree_str += (prefix + f'    ├── Key: {k}\n')
-                tree_str = get_json_struct(data[k], depth+1,
+                tree_str = get_dict_struct(data[k], depth+1,
                             tree_str=tree_str, prefix=(prefix+'    │   '))
             else:
                 tree_str += (prefix + f'    └── Key: {k}\n')
-                tree_str = get_json_struct(data[k], depth+1,
+                tree_str = get_dict_struct(data[k], depth+1,
                             tree_str=tree_str, prefix=(prefix+'        '))
 
     elif isinstance(data, tuple):
         tree_str += (prefix + f'└── TUPLE ({len(data)} elements)\n')
         if len(data)!=0:
-            tree_str=get_json_struct(
+            tree_str=get_dict_struct(
                 data[0], depth+1, tree_str=tree_str, prefix=(prefix + '    '))
 
     elif isinstance(data, list):
         tree_str += (prefix + f'└── LIST ({len(data)} elements)\n')
         if len(data)!=0:
-            tree_str=get_json_struct(
+            tree_str=get_dict_struct(
                 data[0], depth+1, tree_str=tree_str, prefix=(prefix + '    '))
     else:
         tree_str += (prefix + f'└── {data} {type(data)}\n')
